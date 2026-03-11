a2eb2627625fa8162a6845bca719f72922567397
Giuseppe <giuseppe.salv@hotmail.it>
2026-03-11 19:16:51 +0100

    fix errore if

--- PureFunctions/Irpef.py
+++ PureFunctions/Irpef.py
@@ -3,7 +3,7 @@ from Config.Constants import SCAGLIONI_IRPEF
 def calculate_irpef(gross_salary:float) -> float:
     irpef = 0
     for limite, aliquota in SCAGLIONI_IRPEF:
-        if imponibile == gross_salary
+        if imponibile == gross_salary:
             break 
         imponibile = min((limite[1] - limite[0]), gross_salary)
         irpef = irpef + (imponibile * aliquota)
